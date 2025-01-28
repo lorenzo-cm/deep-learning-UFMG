9e867abb74e63051309f37091d4d15283f1c4db1
lorenzo-cm <lorenzocarneirobr@gmail.com>
2025-01-28 09:23:34 -0300

    fix dataset saving size

--- dataset.py
+++ dataset.py
@@ -27,50 +27,59 @@ def build_corpus(size,
                  return_fields=['corpus'],
                  load=True,
                  save=False, 
-                 pickle_file='corpus.pkl', 
                  seed=42):
     
+    pickle_file = f'corpus/{size}-corpus.pkl'
     if os.path.exists(pickle_file) and load and not save:
+        print('-'*40)
+        print('LOADING corpus')
+        print('-'*40)
         corpus, word2idx, idx2word, word_count = load_corpus(size)
-        
-        return_dict = {}
-        if 'corpus' in return_fields:
-            return_dict['corpus'] = corpus
-        
-        if 'word2idx' in return_fields:
-            return_dict['word2idx'] = word2idx
-            
-        if 'idx2word' in return_fields:
-            return_dict['idx2word'] = idx2word
-            
-        if 'word_count' in return_fields:
-            return_dict['word_count'] = word_count
-            
-        return return_dict
-        
-    random.seed(seed)
-    ds = load_dataset("wikimedia/wikipedia", "20231101.en",split='train',streaming=False)
-    rng = np.random.default_rng(seed=seed)
-    idxs = rng.choice(ds.num_rows, size, replace=False)
-    ds = ds.select(idxs)
     
-    word_to_idx={'[UNK]':0}
-    word_counts={}
-    corpus = []
-    stemmer = PorterStemmer()
-    tokenizer = RegexpTokenizer(r'[\w-]+')
-    for x in tqdm(ds):
-        x = x['text']
-        line=[]
-        for word in tokenizer.tokenize(x.lower()):
-            word = stemmer.stem(word)
-            word_to_idx[word] = word_to_idx.get(word,len(word_to_idx))
-            word_counts[word] = word_counts.get(word,0)+1
-            line.append(word)
-        corpus.append(line)
-    idx_to_word = {v:k for k,v in word_to_idx.items()}
-    
-    if save:
+    else:
+        if not os.path.exists(pickle_file):
+            print(f'!!! Filepath {pickle_file} does not exist!')
+            
+        print('-'*40)
+        print('SAVING corpus')
+        print('-'*40)
+        random.seed(seed)
+        ds = load_dataset("wikimedia/wikipedia", "20231101.en",split='train',streaming=False)
+        rng = np.random.default_rng(seed=seed)
+        idxs = rng.choice(ds.num_rows, size, replace=False)
+        ds = ds.select(idxs)
+        
+        word_to_idx={'[UNK]':0}
+        word_counts={}
+        corpus = []
+        stemmer = PorterStemmer()
+        tokenizer = RegexpTokenizer(r'[\w-]+')
+        for x in tqdm(ds):
+            x = x['text']
+            line=[]
+            for word in tokenizer.tokenize(x.lower()):
+                word = stemmer.stem(word)
+                word_to_idx[word] = word_to_idx.get(word,len(word_to_idx))
+                word_counts[word] = word_counts.get(word,0)+1
+                line.append(word)
+            corpus.append(line)
+        idx_to_word = {v:k for k,v in word_to_idx.items()}
+        
         save_corpus(size, corpus, word_to_idx, idx_to_word, word_counts)
+        
     
-    return corpus
+    return_dict = {}
+    
+    if 'corpus' in return_fields:
+        return_dict['corpus'] = corpus
+    
+    if 'word2idx' in return_fields:
+        return_dict['word2idx'] = word2idx
+        
+    if 'idx2word' in return_fields:
+        return_dict['idx2word'] = idx2word
+        
+    if 'word_count' in return_fields:
+        return_dict['word_count'] = word_count
+        
+    return return_dict
